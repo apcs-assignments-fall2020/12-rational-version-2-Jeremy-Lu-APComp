9f9422b1c8945e4f696ada25d4093f3e6ee712f0
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-11-13 06:34:17 +0000

    Transfered code from rational to rational-part-2

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -12,24 +12,25 @@ public class Rational
      * Constructors have the same name as the class
      * Constructors have no return type
      */
-    public Rational(int a, int b) {
+    public Rational(int a, int b)
+    {
         numerator = a;
         denominator = b;
     }
     
     // This method takes two Rationals, add thems up, 
     // and returns a Rational equal to the sum
-    public static Rational add(Rational r, Rational s) {
+    public static Rational add(Rational r, Rational s)
+    {
         int newNumerator = r.numerator*s.denominator + s.numerator*r.denominator;
         int newDenominator = r.denominator*s.denominator; 
         
         Rational x = new Rational(newNumerator, newDenominator);
-        x = simplify(x);
-        return x;
+        return Rational.simplify(x);
     }
 
     // Finds the greatest common factor between a and b
-    public static int greatestCommonFactor(int a, int b) {
+    public static int greatestCommonFactor(int a, int b){
         int gcd = 1;
 
         int k = Math.max(a, b);
@@ -42,6 +43,19 @@ public class Rational
 
         return gcd;
     }
+    public static int lcm(int a, int b){
+        int lcm = 1;
+
+        int k = Math.min(a, b);
+        
+        for(int i = 1; i <= k; i ++){
+            if (a%i == 0 && b%i == 0){
+                lcm = i;
+            }
+        }
+
+        return lcm;
+    }
 
     // This method is given a rational, and returns a simplified version
     // of the input rational
@@ -49,83 +63,107 @@ public class Rational
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r)
     {
-        int factor = greatestCommonFactor(r.numerator, r.denominator);
-        int newNumerator = r.numerator/factor;
-        int newDenominator = r.denominator/factor;
-        Rational x = new Rational(newNumerator, newDenominator);
+        int gcf = greatestCommonFactor(r.numerator, r.denominator);
+        int new_num = r.numerator/gcf;
+        int new_denom = r.denominator/gcf;
+        Rational x = new Rational(new_num, new_denom);
         return x;
     }
 
-    // *****
-    // Here are all of our NON-STATIC methods:
-    // *****
-
-    // Returns whether or not the numerator is greater than or equal
-    // to the denominator
-    public boolean isImproper() {
-       return (this.numerator >= this.denominator);
+    // This method takes two Rationals, subtracts thems up, 
+    // and returns a Rational equal to the difference
+    public static Rational subtract(Rational r, Rational s)
+    {
+        int l_c = lcm(s.denominator, r.denominator);
+        int min_dem = Math.min(r.denominator, s.denominator);
+        int new_num = 0;
+        int new_dem = 0;
+        if(min_dem == r.denominator){
+            new_num = r.numerator * l_c;
+            new_dem = r.numerator * l_c;
+        }
+        if(min_dem == s.denominator){
+            new_num = s.numerator * l_c;
+            new_dem = s.numerator * l_c;
+        }
+        Rational x = new Rational(new_num, new_dem);
+        return Rational.simplify(x);
     }
-
-    // Returns whether or not the Rational is currently simplified
-    // or not
-    public boolean isSimplified() { 
-        return false; // YOUR CODE HERE
-    }
-
-    // Calculates the double value of our Rational
-    public double calculateDecimalValue() { 
-        return 0.0; // YOUR CODE HERE
-    }
-
-    // Returns the Rational we get from raising  the rational number to an integer power
-    public Rational pow(int exponent) {
-        return null; // YOUR CODE HERE
-    }
-
-    // Checks to see if either the numerator or denominator match a given number
-    public boolean matches(int x) {
-        return false; // YOUR CODE HERE
-    }
-
-
-
     
-
-    // Methods you'll write for homework:
-    // Returns whether or not the Rational is a negative number
-    public boolean isNegative() { 
-        return false; // YOUR CODE HERE
+    public static Rational multiply(Rational r, Rational s)
+    {
+        int new_num = r.numerator * s.numerator;
+        int new_dem = r.denominator * s.denominator;
+        Rational x = new Rational(new_num, new_dem);
+        return Rational.simplify(x);
+    }
+    
+    public static Rational divide(Rational r, Rational s)
+    {
+        int new_num = r.numerator * s.denominator;
+        int new_dem = r.denominator * s.numerator;
+        Rational x = new Rational(new_num, new_dem);
+        return Rational.simplify(x);
     }
 
-    // Calculates the reciprocal of a Rational number.
-    // The reciprocal of 3/4 is 4/3, the reciprocal of 1/2 is 2/1
-    public Rational reciprocal() {
-        return null; // YOUR CODE HERE
+    public boolean isNegative()
+    {
+        double result = (double) this.numerator/this.denominator;
+        if(result < 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    // Checks whether the current Rational is the exactly the same as other
-    public boolean equals(Rational other) {
-        return false; // YOUR CODE HERE
+    public boolean equals(Rational r){
+        Rational result_r = Rational.simplify(r);
+        Rational result_this = Rational.simplify(this);
+        if(result_r.numerator == result_this.numerator && result_r.denominator == result_this.denominator){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
+    public Rational reciprocal(){
+        Rational recip = new Rational(this.denominator, this.numerator);
+        return recip;
+    }
 
+    // These following methods are NOT static, we'll talk about them next class!
     // This returns a string representation of a Rational (e.g. "1/2")
     @Override
     public String toString()
     {
         return this.numerator + "/" + this.denominator;
     }
+
+
+    public Rational add(Rational s)
+    {
+        // REPLACE WITH YOUR CODE HERE
+        return null;
+    }
+    
+    public Rational subtract(Rational s)
+    {
+        // REPLACE WITH YOUR CODE HERE
+        return null;
+    }
+    
+    public Rational multiply(Rational s)
+    {
+        // REPLACE WITH YOUR CODE HERE
+        return null;
+    }
+    
+    
+    public Rational divide(Rational s)
+    {
+        // REPLACE WITH YOUR CODE HERE
+        return null;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
